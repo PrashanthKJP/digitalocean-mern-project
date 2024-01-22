4b3b25126cfd040d51a8f423073c111fecc994f3
PrashanthKJP <prashanthprash969@gmail.com>
2024-01-22 19:06:56 +0530

    first commit

---
+++ fancyserver/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//middleware
+app.use(express.json());
+
+app.use(cors());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Prashanth:FancyNumber@fancynumber.ipudrdg.mongodb.net/",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("DATA-BASE CONNECTION SUCCESSFULLY");
+  } catch (error) {
+    console.log(error, "DATA BASE not connected");
+  }
+};
+//database connection
+connectDB();
+
+//route's
+app.use("/api", require("./routes/userRoute"));
+app.use("/api", require("./routes/numberRoutes"));
+app.use("/api", require("./routes/orderRoute"));
+app.use("/api", require("./routes/filterRoute"));
+
+app.listen(8888, () => {
+  console.log(`server is running on port ${8888}`);
+});
