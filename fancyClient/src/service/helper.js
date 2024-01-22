0bd757dba76792e874ab9eb3e40ee1253b2fdea4
PrashanthKJP <prashanthprash969@gmail.com>
2024-01-22 22:46:57 +0530

    update base URL

--- fancyClient/src/service/helper.js
+++ fancyClient/src/service/helper.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://fancy-number.onrender.com";
 // export const BASE_URL = "http://localhost:8888";
-export const BASE_URL = "http://134.209.147.163";
+export const BASE_URL = "http://68.183.83.166";
