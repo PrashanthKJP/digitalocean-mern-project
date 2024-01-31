3d74cb240c3e1af115fcf60490830144224a15ea
PrashanthKJP <prashanthprash969@gmail.com>
2024-01-31 08:49:04 +0530

    update base url

--- fancyClient/src/service/helper.js
+++ fancyClient/src/service/helper.js
@@ -1,4 +1,3 @@
-// export const BASE_URL = "https://fancy-number.onrender.com";
-export const BASE_URL = "http://localhost:8888";
-// export const BASE_URL = "https://68.183.83.166";
-// export const BASE_URL = "https://www.permanentvipfancynumbers.com";
+// export const BASE_URL = "http://localhost:8888";
+
+export const BASE_URL = "https://www.permanentvipfancynumbers.com";
