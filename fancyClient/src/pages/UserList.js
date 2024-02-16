fe224f8512fb69f39cac034043e43cd21181851e
PrashanthKJP <prashanthprash969@gmail.com>
2024-02-16 18:34:01 +0530

    update userList download excel and total user

--- fancyClient/src/pages/UserList.js
+++ fancyClient/src/pages/UserList.js
@@ -15,6 +15,7 @@ import { format } from "timeago.js";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import ExcelJS from "exceljs";
 
 const UserList = () => {
   const userState = useSelector((state) => state.getAllUsersReducer);
@@ -46,9 +47,93 @@ const UserList = () => {
     alert("you cannot delete Admin account");
   };
 
+  const exportExcelFile = () => {
+    const workbook = new ExcelJS.Workbook();
+    const sheet = workbook.addWorksheet("My Sheet");
+    sheet.properties.defaultRowHeight = 50;
+
+    sheet.getRow(1).border = {
+      top: { style: "thick", color: { argb: "FF808080" } }, // Black color
+      left: { style: "thick", color: { argb: "FF808080" } }, // Black color
+      bottom: { style: "thick", color: { argb: "FF808080" } }, // Black color
+      right: { style: "thick", color: { argb: "FF808080" } }, // Black color
+    };
+
+    sheet.getRow(1).fill = {
+      type: "pattern",
+      pattern: "solid",
+      fgColor: { argb: "ffff6347" }, // Red color
+    };
+
+    sheet.getRow(1).font = {
+      name: "Comic Sans MS",
+      family: 4,
+      size: 16,
+      bold: true,
+    };
+
+    sheet.columns = [
+      {
+        header: "Id",
+        key: "id",
+        width: 20,
+      },
+      {
+        header: "Name",
+        key: "name",
+        width: 20,
+      },
+      {
+        header: "Number",
+        key: "number",
+        width: 20,
+      },
+      {
+        header: "IsAdmin",
+        key: "isAdmin",
+        width: 10,
+      },
+      {
+        header: "CreatedAt",
+        key: "createdAt",
+        width: 15,
+      },
+    ];
+
+    letestUser?.map(async (product, index) => {
+      sheet.addRow({
+        id: product?._id,
+        name: product?.name,
+        number: product?.number,
+        isAdmin: product?.isAdmin,
+        createdAt: product?.createdAt,
+      });
+    });
+
+    workbook.xlsx.writeBuffer().then(function (data) {
+      const blob = new Blob([data], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+      const url = window.URL.createObjectURL(blob);
+      const anchor = document.createElement("a");
+      anchor.href = url;
+      anchor.download = "download.xlsx";
+      anchor.click();
+      window.URL.revokeObjectURL(url);
+    });
+  };
+
   return (
     <Container fluid>
-      <Navbar expand="lg"></Navbar>
+      <Navbar>
+        <Navbar.Brand>Total {letestUser.length} No Of User's</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Button onClick={exportExcelFile}>download excel</Button>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
       <Table striped="columns">
         <Thead>
           <Tr>
